dfe2d73a21cd3dc56cc27ed82377ba078326bd0d
Anh <qanh171002@gmail.com>
2025-07-27 12:51:25 +0700

    feat: add User model

---
+++ models/user.js
@@ -0,0 +1,36 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/db");
+
+const User = sequelize.define(
+  "User",
+  {
+    id: {
+      type: DataTypes.UUIDV4,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: { notEmpty: true, len: [2, 20] },
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: { isEmail: true },
+    },
+    passwordHash: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    timestamps: true,
+    createdAt: "createdAt",
+    updatedAt: "updatedAt",
+    tableName: "users",
+  }
+);
+
+module.exports = User;
