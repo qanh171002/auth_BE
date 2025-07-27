c6bc8d28fe013325f09881f8848d0e0c8860eb76
Anh <qanh171002@gmail.com>
2025-07-27 15:43:01 +0700

    feat: add user controller with sign up, log in

---
+++ controllers/authController.js
@@ -0,0 +1,77 @@
+const brcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("../models/user.js");
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+exports.signup = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) {
+      return res
+        .status(400)
+        .json({ success: false, message: "All fields are required" });
+    }
+
+    const existingUser = await User.findOne({ where: { email } });
+    if (existingUser) {
+      return res.status(409).json({
+        success: false,
+        message: "User with this email already exists",
+      });
+    }
+
+    const passwordHash = await brcrypt.hash(password, 10);
+    const user = await User.create({ name, email, passwordHash });
+    return res.status(201).json({
+      success: true,
+      message: "User created successfully",
+      user: { id: user.id, name: user.name, email: user.email },
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ success: false, message: "All fields are required" });
+    }
+
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    const isPasswordValid = await brcrypt.compare(password, user.passwordHash);
+    if (!isPasswordValid) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid credentials",
+      });
+    }
+
+    const token = jwt.sign({ id: user.id }, JWT_SECRET, { expiresIn: "1h" });
+    return res.status(200).json({
+      success: true,
+      message: "Login successful",
+      token,
+      user: { id: user.id, name: user.name, email: user.email },
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
