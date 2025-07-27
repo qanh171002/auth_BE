f1cf9f803dbd7104b5f1b37dad61093f9a9b80c1
Anh <qanh171002@gmail.com>
2025-07-27 22:44:32 +0700

    chore: update CORS config with origin

--- server.js
+++ server.js
@@ -8,7 +8,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://auth-fe-livid.vercel.app"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use("/api", userRoute);
 
